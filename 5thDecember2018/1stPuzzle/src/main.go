c06bc398cba59cd0c5ce7cdf96a21b881b11ed8f
Arco Gelderblom <arco.gelderblom@hotmail.com>
2018-12-05 23:56:27 +0100

    Completed first puzzle of 5 december

--- 5thDecember2018/1stPuzzle/src/main.go
+++ 5thDecember2018/1stPuzzle/src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 func checkError(err error) {
@@ -15,5 +16,13 @@ func main() {
 	file, err := ioutil.ReadFile("../input.txt")
 	checkError(err)
 	polymer := string(file)
-	fmt.Println(polymer)
+	
+	for i := 0; i < len(polymer)-1; i++ {
+		if polymer[i] == (polymer[i+1]+32) || polymer[i] == (polymer[i+1]-32) {
+			stringToDelete := string(polymer[i]) + string(polymer[i+1])
+			polymer = strings.Replace(polymer, stringToDelete, "", -1)
+			i = 0 // start over with checking since something is deleted
+		}
+	}
+	fmt.Println("Amount of units left in polymer:", len(polymer))
 }
