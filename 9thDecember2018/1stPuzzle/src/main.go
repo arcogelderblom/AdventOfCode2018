29778c519bcc433683abb721989515c88b7642bb
Arco Gelderblom <arco.gelderblom@hotmail.com>
2018-12-12 08:31:46 +0100

    Completed the first puzzle

--- 9thDecember2018/1stPuzzle/src/main.go
+++ 9thDecember2018/1stPuzzle/src/main.go
@@ -13,71 +13,86 @@ func checkError(err error) {
 	}
 }
 
-type Node struct {
-	index int
-	childrenNum int
-	children []Node
-	metadataNum int
-	metadata []int
+func insert(list []int, insertion int, index int) []int {
+	var slice1 = make([]int, len(list[:index]))
+	var slice2 = make([]int, len(list[index:]))
+	copy(slice1, list[:index])
+	copy(slice2, list[index:])
+	return append(append(slice1, insertion), slice2...)
 }
 
-func convertToInt(list []string) []int {
-	returnArray := []int{}
-
-	for index := range list {
-		number, err := strconv.Atoi(list[index])
-		checkError(err)
-
-		returnArray = append(returnArray, number)
+func addMarble(currentList []int, currentIndex int, marble int) ([]int, int) {
+	if marble == 1 {
+		currentList = append(currentList, marble)
+		currentIndex += 1
+	} else {
+		if currentIndex == len(currentList) - 1 {
+			currentIndex = 1
+			currentList = insert(currentList, marble, currentIndex)
+			currentIndex = 1
+		} else if currentIndex + 1 ==  len(currentList) - 1 {
+			currentList = append(currentList, marble)
+			currentIndex += 2
+		} else {
+			currentIndex += 2
+			currentList = insert(currentList, marble, currentIndex)
+		}
 	}
-	return returnArray
+	return currentList, currentIndex
 }
 
-func getNode(index int, split []int) Node {
-	node := Node{index: index, childrenNum: split[index], metadataNum: split[index+1]}
-
-	offset := node.index + 2
-
-	for i := 0; i < node.childrenNum ; i++ {
-		childNode := getNode(offset, split)
-		node.children = append(node.children, childNode)
-		offset = offset + getLength(childNode)
+func countScore(gameList []int, playerList []int, playersTurn int, marble int, currentIndex int) ([]int, []int, int) {
+	currentIndex -= 7 // what if this is negative
+	if currentIndex < 0 {
+		currentIndex = len(gameList) + currentIndex
 	}
-
-	for i := 0; i < node.metadataNum ; i++ {
-		node.metadata = append(node.metadata,split[offset + i])
-	}
-
-	return node
+	playerList[playersTurn-1] += marble + gameList[currentIndex]
+	gameList = append(gameList[:currentIndex], gameList[currentIndex+1:]...)
+	return gameList, playerList, currentIndex
 }
 
-func getLength(node Node) int {
-	length := 2
-	for i := 0; i < node.childrenNum ; i++ {
-		length = length + getLength(node.children[i])
+func playGame(amountOfPlayers int, lastMarbleVal int) []int {
+	var playerList = make([]int, amountOfPlayers) // list of players where index is the playernumber and the value is the score
+	gameField := []int{0}
+
+	playerTurn := 1
+	index := 0
+	for i := 1; i <= lastMarbleVal; i++ {
+		if i % 23 == 0 {
+			gameField, playerList, index = countScore(gameField, playerList, playerTurn, i, index)
+		} else {
+			gameField, index = addMarble(gameField, index, i)
+		}
+		playerTurn += 1
+		if playerTurn > amountOfPlayers {
+			playerTurn = 1
+		}
 	}
-	length = length + node.metadataNum
-	return length
+	return playerList
 }
 
-func sum(node Node) int {
-	total := 0
-	for _,v := range node.children {
-		total = total + sum(v)
+func getMaxScore(scores []int) (int, int){
+	maxScore := 0
+	bestPlayer := 0
+	for i := range scores {
+		if scores[i] > maxScore {
+			maxScore = scores[i]
+			bestPlayer = i + 1
+		}
 	}
-	for _,v := range node.metadata {
-		total = total + v
-	}
-	return total
+	return maxScore, bestPlayer
 }
 
 func main() {
 	file, err := ioutil.ReadFile("../input.txt")
 	checkError(err)
-	data := strings.Split(string(file), " ")
-	dataInInt := convertToInt(data)
+	amountOfPlayers, err := strconv.Atoi(strings.Split(string(file), " ")[0])
+	checkError(err)
+	lastMarble, err := strconv.Atoi(strings.Split(string(file), " ")[6])
+	checkError(err)
 
-	nodes := getNode(0, dataInInt) // remember to split multiple child nodes
+	players := playGame(amountOfPlayers, lastMarble)
+	maxScore, bestPlayer := getMaxScore(players)
 
-	fmt.Println("The sum of all metadata is", sum(nodes))
+	fmt.Println("The best player is", bestPlayer, "with a score of", maxScore)
 }
